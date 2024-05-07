570182930f991c26d5423a2c6c434c682ef7671d
jbalinas <108829766+jbalinas@users.noreply.github.com>
2024-05-06 18:57:09 -0700

    Storage works for entry/quit/reopen

---
+++ lib/models/journal_entry.g.dart
@@ -0,0 +1,53 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'journal_entry.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class JournalEntryAdapter extends TypeAdapter<JournalEntry> {
+  @override
+  final int typeId = 0;
+
+  @override
+  JournalEntry read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return JournalEntry(
+      gratitudeText: fields[0] as dynamic,
+      highlightText: fields[1] as dynamic,
+      createdAt: fields[4] as DateTime,
+      updatedAt: fields[3] as DateTime,
+      uuid: fields[2] as String,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, JournalEntry obj) {
+    writer
+      ..writeByte(5)
+      ..writeByte(0)
+      ..write(obj.gratitudeText)
+      ..writeByte(1)
+      ..write(obj.highlightText)
+      ..writeByte(2)
+      ..write(obj.uuid)
+      ..writeByte(3)
+      ..write(obj.updatedAt)
+      ..writeByte(4)
+      ..write(obj.createdAt);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is JournalEntryAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
